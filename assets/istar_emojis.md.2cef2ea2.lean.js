08f8d5aa7dfbed552374317cbb2a520089e8e098
lichao <184078718@qq.com>
2022-12-27 16:41:51 +0800

    deploy

---
+++ assets/istar_emojis.md.2cef2ea2.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as l,o as a,d as e}from"./app.1552690c.js";const h=JSON.parse('{"title":"emojis","description":"","frontmatter":{},"headers":[],"relativePath":"istar/emojis.md"}'),n={name:"istar/emojis.md"},o=e("",2),_=[o];function r(t,s,c,m,g,d){return a(),l("div",null,_)}const p=i(n,[["render",r]]);export{h as __pageData,p as default};
